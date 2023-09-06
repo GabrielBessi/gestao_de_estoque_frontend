fcedeb8f365c78fdba9ff577042758a010ed350d
Gabriel Bessi <tecbessi@gmail.com>
2023-09-06 17:06:44 -0300

    fix<form-login>: Mudança na forma de realizar as requisições

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,10 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
 import { GlobalStyle } from "./style/global";
-import { QueryClientProvider } from "react-query";
-import queryClient from "./client/query";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <GlobalStyle />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
