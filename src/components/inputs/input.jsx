ba7681506a02fba4a2162b8ef0c3937b96a0a99a
Gabriel Bessi <tecbessi@gmail.com>
2023-09-06 17:11:19 -0300

    feat<form-login>: Adição de mais uma informação de input

--- src/components/inputs/input.jsx
+++ src/components/inputs/input.jsx
@@ -1,12 +1,17 @@
 import { InputForm } from "./styles";
 
-function Input({ type, placeholder, label, dataUser }) {
+function Input({ type, placeholder, label, dataUser, value }) {
   return (
     <InputForm>
       <label style={{ color: "black" }} htmlFor="">
         {label}
       </label>
-      <input type={type} placeholder={placeholder} {...dataUser} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        {...dataUser}
+        value={value}
+      />
     </InputForm>
   );
 }
