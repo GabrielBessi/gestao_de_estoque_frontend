e588bb21c4a724199a7ece092a54d5ef599d5133
Gabriel Bessi <tecbessi@gmail.com>
2023-09-06 17:07:12 -0300

    feat<form-login>: rota da home page assim que é logado

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/login/login";
 import RegisterPage from "../pages/register/register";
+import HomePage from "../pages/home";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "register",
     element: <RegisterPage />,
+  },
+  {
+    path: "home",
+    element: <HomePage />,
   },
 ]);
 
