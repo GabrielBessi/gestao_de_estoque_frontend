0932442a96ed983b1cbb8a60c9ff659f4c986175
Gabriel Bessi <tecbessi@gmail.com>
2023-09-16 06:16:27 -0300

    fix<form-login> implantação do useEffect

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,36 @@
+import { useEffect } from "react";
+import { loginStore } from "../../store/loginStore";
 import { SectionStyle } from "./style";
 import { TfiMenu } from "react-icons/tfi";
+import api from "../../services/api";
 
 function Header() {
+  const { name } = loginStore((state) => state.user);
+
+  useEffect(() => {
+    const token = localStorage.getItem("estoque:token");
+
+    async function loadUser() {
+      if (token) {
+        try {
+          api.defaults.headers.authorization = `Bearer ${token}`;
+          const { data } = await api.get("/users");
+
+          console.log("---data--->", data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    loadUser();
+  }, []);
+
   return (
     <SectionStyle>
       <div>
         <img src="" alt="" />
-        <span>Bom dia macho</span>
+        <span>Bom dia {name}</span>
       </div>
       <div>
         <span>
--- src/store/loginStore.jsx
+++ src/store/loginStore.jsx
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 
 export const loginStore = create((set) => ({
-  user: [],
+  user: {},
   registerUser: [],
 
   login: (user) => {
-    set(() => ({ user: [user] }));
+    set(() => ({ user: user }));
   },
 
   newRegister: (user) => {
